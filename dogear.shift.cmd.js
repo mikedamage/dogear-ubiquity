207239b5e8d823ff4b09eafe9676c691b9785ce7
Mike Green <mike.is.green@gmail.com>
2009-04-07 16:47:27 -0400

    created dogear.shift command to return an item from the beginning of the list

---
+++ dogear.shift.cmd.js
@@ -0,0 +1,26 @@
+CmdUtils.CreateCommand({
+	name: "dogear.shift",
+	author: {
+		name: "Mike Green",
+		email: "mike.is.green@gmail.com",
+		homepage: "http://www.astronautcrossing.com"
+	},
+	license: "GNU General Public License",
+	_preferenceKey: "extensions.ubiquity.dogear.pageList",
+	_listExists: function() {
+		return Application.prefs.has(this._preferenceKey);
+	},
+	_getListJSON: function() {
+		return Utils.decodeJson(Application.prefs.get(this._preferenceKey).value);
+	},
+	_saveList: function(obj) {
+		var list = Utils.encodeJson(obj);
+		return Application.prefs.setValue(this._preferenceKey, list);
+	},
+	execute: function(input) {
+		var list = this._getListJSON();
+		var pop = list.shift();
+		this._saveList(list);
+		Utils.openUrlInBrowser(pop.url);
+	}
+});
