5fd4c810e2f5118af865977c14c602ac4e724428
Mike Green <mike.is.green@gmail.com>
2009-04-07 21:48:17 -0400

    updated the dogear list command

--- dogear.list.cmd.js
+++ dogear.list.cmd.js
@@ -15,14 +15,20 @@ CmdUtils.CreateCommand({
 		return Utils.decodeJson(Application.prefs.get(this._preferenceKey).value);
 	},
 	takes: {"number": /\d+/},
-	preview: function(pblock) {
+	preview: function(pblock, input) {
+		var itemNumber = parseInt(input.text) - 1;
 		if (this._listExists()) {
 			var list = this._getListJSON();
 			var itemTemplate = "<strong>${title}</strong><br/><img src=\"${snapshot}\" /><br/>${url}";
 			pblock.innerHTML = "<strong>DogEar List:</strong>\n<div>\n";
-			for (i=0; i < list.length; i++) {
-				pblock.innerHTML += CmdUtils.renderTemplate(itemTemplate, {title: list[i].title, url: list[i].url, snapshot: list[i].snapshot});
+			if (input.text != "") {
+				pblock.innerHTML += CmdUtils.renderTemplate(itemTemplate, {title: list[itemNumber].title, url: list[itemNumber].url, snapshot: list[itemNumber].snapshot});
+			} else {
+				for (i=0; i < list.length; i++) {
+					pblock.innerHTML += CmdUtils.renderTemplate(itemTemplate, {title: list[i].title, url: list[i].url, snapshot: list[i].snapshot});
+				}
 			}
+			
 			pblock.innerHTML += "</div>";
 		} else {
 			pblock.innerHTML = "<strong>No Items in DogEar list</strong>";
@@ -31,7 +37,8 @@ CmdUtils.CreateCommand({
 	execute: function(input) {
 		var item = parseInt(input.text) - 1;
 		var list = this._getListJSON();
-		displayMessage(list[item].url);
+		displayMessage(list[item].url + " copied to clipboard.");
+		CmdUtils.copyToClipboard(list[item].url);
 		//list.delete(item);
 	}
 });
